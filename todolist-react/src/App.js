454e03c59aa1592814ba565ad4aabebfb34c9369
EdMkn <nmanyim@rocketmail.com>
2023-08-23 17:07:04 +0200

    Componentizing todo

--- todolist-react/src/App.js
+++ todolist-react/src/App.js
@@ -1,4 +1,13 @@
+import Todo from "./components/Todo";
+
 function App(props) {
+  const taskList = props.tasks?.map((task) => (
+    <Todo 
+      id={task.id} 
+      name={task.name} 
+      completed={task.completed} 
+      key={task.id}/>
+  ));
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -22,18 +31,18 @@ function App(props) {
       <div className="filters btn-group stack-exception">
         <button type="button" className="btn toggle-btn" aria-pressed="true">
           <span className="visually-hidden">Afficher </span>
-          <span>Toutes les</span>
-          <span className="visually-hidden"> tâches</span>
+          <span>Toutes</span>
+          <span className="visually-hidden">les tâches</span>
         </button>
         <button type="button" className="btn toggle-btn" aria-pressed="false">
           <span className="visually-hidden">Afficher </span>
-          <span>les tâches</span>
-          <span className="visually-hidden"> actives</span>
+          <span className="visually-hidden">les tâches</span>
+          <span > actives</span>
         </button>
         <button type="button" className="btn toggle-btn" aria-pressed="false">
           <span className="visually-hidden">Afficher </span>
-          <span>les tâches</span>
-          <span className="visually-hidden"> complêtes</span>
+          <span className="visually-hidden">les tâches</span>
+          <span > complêtes</span>
         </button>
       </div>
       <h2 id="list-heading">3 tâches restantes</h2>
@@ -41,54 +50,7 @@ function App(props) {
         role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-0" type="checkbox" defaultChecked={true} />
-            <label className="todo-label" htmlFor="todo-0">
-              Balayer
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Editer <span className="visually-hidden">Balayer</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Supprimer <span className="visually-hidden">Balayer</span>
-            </button>
-          </div>
-        </li>
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-1" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-1">
-              Nettoyer
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Editer <span className="visually-hidden">Nettoyer</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Supprimer <span className="visually-hidden">Nettoyer</span>
-            </button>
-          </div>
-        </li>
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-2" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-2">
-            Répeter
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Editer <span className="visually-hidden">Répeter</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Supprimer <span className="visually-hidden">Répeter</span>
-            </button>
-          </div>
-        </li>
+        {taskList}
       </ul>
     </div>
   );
--- todolist-react/src/index.js
+++ todolist-react/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const DATA = [
+  { id: "todo-0", name: "Balayer", completed: true },
+  { id: "todo-1", name: "Nettoyer", completed: false },
+  { id: "todo-2", name: "Répeter", completed: false },
+];
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA}/>
   </React.StrictMode>
 );
 
